a5c5a49e9630d41d72de43d5c2bd2488cbca7c89
Manahil-04 <manahilmushtaq004@gmail.com>
2024-02-18 08:25:06 +0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', function() {
+    const timelineItems = document.querySelectorAll('.timeline-item');
+    timelineItems.forEach(item => {
+      if (isElementInViewport(item)) {
+        item.classList.add('show');
+      }
+    });
+  });
+  
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
